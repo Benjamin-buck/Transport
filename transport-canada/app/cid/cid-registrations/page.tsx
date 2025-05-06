97cf57b4214f44855a06cb2eb207fe8bcb9df5bc
Benjamin-buck <164558323+Benjamin-buck@users.noreply.github.com>
2025-05-06 14:28:57 -0400

    Adds UI changes to CID Dashboard

--- transport-canada/app/cid/cid-registrations/page.tsx
+++ transport-canada/app/cid/cid-registrations/page.tsx
@@ -1,13 +1,5 @@
 import ServiceMenu from "@/components/ServiceMenu";
 import Title from "@/components/Title";
-import {
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from "@/components/ui/table";
 import React from "react";
 
 const CIDDashboard = () => {
@@ -19,36 +11,6 @@ const CIDDashboard = () => {
       <div className="col-span-9">
         <h2 className="text-xl">Registrations</h2>
         <Title>Client Identification Database (CID)</Title>
-        <div className="mt-6">
-          <Table>
-            <TableHeader>
-              <TableRow>
-                <TableHead className="w-[100px]">Service</TableHead>
-                <TableHead>Company Registered</TableHead>
-                <TableHead>Registration Date</TableHead>
-                <TableHead>Expiry Date</TableHead>
-                <TableHead>Registration Status</TableHead>
-                <TableHead className="text-right">Actions</TableHead>
-              </TableRow>
-            </TableHeader>
-            <TableBody>
-              <TableRow>
-                <TableCell className="font-medium">001920</TableCell>
-                <TableCell>Walmart Inc.</TableCell>
-                <TableCell>10/20/2024</TableCell>
-                <TableCell>10/20/2025</TableCell>
-                <TableCell>
-                  <span className="bg-green-200 px-4 py-1 text-green-600 border-l-4 border-green-600">
-                    Registered
-                  </span>
-                </TableCell>
-                <TableCell className="text-right text-blue-500 underline">
-                  View / Modify
-                </TableCell>
-              </TableRow>
-            </TableBody>
-          </Table>
-        </div>
       </div>
     </div>
   );
--- transport-canada/app/cid/dashboard/page.tsx
+++ transport-canada/app/cid/dashboard/page.tsx
@@ -1,14 +1,29 @@
-import ServiceMenu from "@/components/ServiceMenu";
 import Title from "@/components/Title";
 import React from "react";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import Link from "next/link";
+import { ChevronLeft } from "lucide-react";
 
 const CIDDashboard = () => {
   return (
-    <div className="mx-auto container my-12 grid grid-cols-12">
-      <div className="col-span-3">
-        <ServiceMenu />
-      </div>
-      <div className="col-span-9">
+    <div className="mx-auto container mt-6">
+      <div>
+        <div className="mb-2 flex gap-2 items-center">
+          <Link href="/organizations/sample-organization" className="underline">
+            Home
+          </Link>
+          <ChevronLeft size={15} />
+          <Link href="/" className="underline">
+            Client Identification Database
+          </Link>
+        </div>
         <h2 className="text-xl">Dashboard</h2>
         <Title>Client Identification Database (CID)</Title>
         <div className="card">
@@ -19,15 +34,42 @@ const CIDDashboard = () => {
             your companies in one spot.
           </p>
         </div>
+        <div className="mt-6 card ">
+          <div className="flex mb-6 items-center justify-between">
+            <h2 className="text-2xl font-semibold">My Registrations</h2>
+            <button className="button-dark">New CID Registration</button>
+          </div>
+          <Table>
+            <TableHeader>
+              <TableRow>
+                <TableHead className="w-[100px]">Service</TableHead>
+                <TableHead>Company Registered</TableHead>
+                <TableHead>Registration Date</TableHead>
+                <TableHead>Expiry Date</TableHead>
+                <TableHead>Registration Status</TableHead>
+                <TableHead>Last Modified</TableHead>
+                <TableHead className="text-right">Actions</TableHead>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              <TableRow>
+                <TableCell className="font-medium">001920</TableCell>
+                <TableCell>Walmart Inc.</TableCell>
+                <TableCell>10/20/2024</TableCell>
+                <TableCell>10/20/2025</TableCell>
+                <TableCell>
+                  <span className="bg-green-200 px-4 py-1 text-green-600 border-l-4 border-green-600">
+                    Registered
+                  </span>
+                </TableCell>
+                <TableCell>10/20/2025</TableCell>
 
-        <div className="grid grid-cols-12 gap-x-6">
-          <div className="card col-span-6">
-            <h2 className="text-xl font-semibold">Latest Activity</h2>
-          </div>
-          <div className="card col-span-6">
-            <h2 className="text-xl font-semibold">Reminders</h2>
-            <div></div>
-          </div>
+                <TableCell className="text-right text-blue-500 underline">
+                  View / Modify
+                </TableCell>
+              </TableRow>
+            </TableBody>
+          </Table>
         </div>
       </div>
     </div>
