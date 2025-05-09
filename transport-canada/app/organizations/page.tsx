533666cc73a77c76c4ab36812f48e73e7240371b
Benjamin-buck <164558323+Benjamin-buck@users.noreply.github.com>
2025-05-09 11:39:19 -0400

    Makes modifications to the My Organization UI.

--- transport-canada/app/organizations/page.tsx
+++ transport-canada/app/organizations/page.tsx
@@ -14,22 +14,15 @@ import Link from "next/link";
 const page = () => {
   return (
     <div className="mb-10  mx-auto container ">
-      <div className="mt-8 flex justify-between items-center">
-        <Title>My Organizations</Title>
-        {/* <p className="mt-6">Welcome to the Dangerous Goods portal.</p> */}
-        <Link href="/organizations/new" className="button-dark">
-          New Organization
-        </Link>
-      </div>
-      <div>
-        <InfoMessage>
-          <p>
-            The following is a list of all organizations associated to your
-            account.
-          </p>
-        </InfoMessage>
-      </div>
-      <div className="mx-auto container mt-12">
+      <div className="mx-auto container card">
+        <div className="flex justify-between items-center">
+          <Title>My Organizations</Title>
+          {/* <p className="mt-6">Welcome to the Dangerous Goods portal.</p> */}
+          <Link href="/organizations/new" className="button-dark">
+            New Organization
+          </Link>
+        </div>
+
         <Table>
           <TableHeader>
             <TableRow>
@@ -63,44 +56,43 @@ const page = () => {
           </TableBody>
         </Table>
       </div>
-      <div className="mt-8 flex justify-between items-center">
-        <Title>Pending Organizations</Title>
-      </div>
-      <div>
-        <div className="mx-auto container mt-12">
-          <Table>
-            <TableHeader>
-              <TableRow>
-                <TableHead className="w-[150px]">ID</TableHead>
-                <TableHead>Legal Name</TableHead>
-                <TableHead>Operating Name</TableHead>
-                <TableHead>Address</TableHead>
-                <TableHead>Status</TableHead>
-                <TableHead>Actions</TableHead>
-              </TableRow>
-            </TableHeader>
-            <TableBody>
-              {pendingOrganizations.map((organization) => (
-                <TableRow key={organization.legalName}>
-                  <TableCell className="font-medium text-blue-400 underline">
-                    {organization.id}
-                  </TableCell>
-                  <TableCell>{organization.legalName}</TableCell>
-                  <TableCell>{organization.operatingName}</TableCell>
-                  <TableCell>{organization.address}</TableCell>
-                  <TableCell>
-                    <span className="bg-orange-200 text-orange-600 border-l-4 border-orange-600 px-4 py-0.5">
-                      {organization.status}
-                    </span>
-                  </TableCell>
-                  <TableCell className="underline text-blue-400">
-                    <Link href="/organizations/sample-organization">View</Link>
-                  </TableCell>
-                </TableRow>
-              ))}
-            </TableBody>
-          </Table>
+
+      <div className="mx-auto container mt-12 card">
+        <div className="flex justify-between items-center">
+          <Title>Pending Organizations</Title>
         </div>
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead className="w-[150px]">ID</TableHead>
+              <TableHead>Legal Name</TableHead>
+              <TableHead>Operating Name</TableHead>
+              <TableHead>Address</TableHead>
+              <TableHead>Status</TableHead>
+              <TableHead>Actions</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {pendingOrganizations.map((organization) => (
+              <TableRow key={organization.legalName}>
+                <TableCell className="font-medium text-blue-400 underline">
+                  {organization.id}
+                </TableCell>
+                <TableCell>{organization.legalName}</TableCell>
+                <TableCell>{organization.operatingName}</TableCell>
+                <TableCell>{organization.address}</TableCell>
+                <TableCell>
+                  <span className="bg-orange-200 text-orange-600 border-l-4 border-orange-600 px-4 py-0.5">
+                    {organization.status}
+                  </span>
+                </TableCell>
+                <TableCell className="underline text-blue-400">
+                  <Link href="/organizations/sample-organization">View</Link>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
       </div>
     </div>
   );
